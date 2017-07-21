c3583cd9caa8122eac4498e661f6200fa4ee9b15
Sokly <sokly@yoolk.com>
2017-07-21 15:46:55 +0700

    chore(): enh style event detail

--- src/pages/show-event/show-event.ts
+++ src/pages/show-event/show-event.ts
@@ -12,13 +12,14 @@ export class ShowEventPage {
   private event: any;
   private url = '';
   private creatorPicture: any;
-  private creatorName: any;
+  private creatorName: any = 'Sopheak Kim';
   private profileLink: any;
 
   constructor(public navParams: NavParams, private endpoint: Endpoint, private facebook: Facebook,) {
     this.event = navParams.get('event');
     this.url = endpoint.url;
     this.creatorPicture = "https://graph.facebook.com/" + this.event['user_id'] + "/picture?type=small";
+    // this.creatorPicture = 'https://scontent.fpnh4-1.fna.fbcdn.net/v/t1.0-9/13001255_1133639440000791_989390100380862014_n.jpg?oh=2eb1a18499cd7c4bec944b35a5090b37&oe=59F3F553';
     this.profileLink = "https://www.facebook.com/profile.php?id=" + this.event['user_id'];
   }
 
@@ -29,7 +30,7 @@ export class ShowEventPage {
   setCreatorName() {
     let self = this;
     this.facebook.api("/" + this.event['user_id'] +"?fields=name" , []).then(function(user) {
-      self.creatorName = user.name;
+      self.creatorName = user.name || 'Sopheak Kim';
     })
   }
 }
--- src/providers/endpoint.ts
+++ src/providers/endpoint.ts
@@ -3,8 +3,8 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class Endpoint {
-  public url: any = "https://4e8f8be0.ngrok.io";
-  public api:any = "https://4e8f8be0.ngrok.io/api/v1/";
+  public url: any = "https://b0af9c6b.ngrok.io";
+  public api:any = "https://b0af9c6b.ngrok.io/api/v1/";
   constructor() {
   }
 
